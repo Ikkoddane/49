b5b56c5d4cd71218bee49c078c08a237023c34b6
Iskander <iscanderakhmetov@gmail.com>
2024-05-07 22:34:19 +0600

    Created class Deck, Updated class Card

--- script.js
+++ script.js
@@ -1,3 +1,36 @@
-class Card{
-    
+const SUITS = ["♠", "♥", "♦", "♣"];
+const RANKS = ["2", "3", "4", "5", "6", "7", "8", "9", "10", "J", "Q", "K", "A"];
+
+class Card {
+    constructor(suit, rank) {
+        this.suit = suit;
+        this.rank = rank;
+    }
+
+    display() {
+        console.log(`${this.suit} ${this.rank}`)
+    }
+}
+
+class Deck {
+    constructor() {
+        this.cards = [];
+        for (let suit of SUITS) {
+            for (let rank of RANKS) {
+                this.cards.push(new Card(suit, rank))
+            }
+        }
+        this.shuffle()
+    }
+
+    shuffle() {
+        for (let i = this.cards.length - 1; i > 0; i--){
+            const j = Math.floor(Math.random() * (i + 1));
+            [this.cards[i], this.cards] = [this.cards[j], this.cards[i]]
+        }
+    }
+
+    takeCard(){
+        return this.cards.pop()
+    }
 }
